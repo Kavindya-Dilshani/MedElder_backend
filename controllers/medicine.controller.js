0cdad354d8f6d7628daba3dec3341612ccfdea22
Kavindya Dilshani <ranaweerakdilshani@gmail.com>
2024-07-30 14:17:05 +0530

    update save function

--- controllers/medicine.controller.js
+++ controllers/medicine.controller.js
@@ -9,9 +9,6 @@ const getAllMedicine = async (req, res) => {
   }
 };
 
-
-
-
 const saveMedicine = async (req, res) => {
   console.log(req.body);
   try {
@@ -24,33 +21,38 @@ const saveMedicine = async (req, res) => {
       doses,
       reminder,
     } = req.body;
+
     if (
       !userId ||
       !medicineName ||
       !selectedMedicine ||
       !amount ||
       !frequency ||
-      !doses  ||
+      !doses ||
       !reminder
     ) {
       return res.status(400).json({ message: "Data not provided" });
     }
 
-    const getMedicine = await Medicine.findOne({ userId });
-
-    if (!getMedicine) {
-      await Medicine.create({
-        userId,
-        medicineName,
-        selectedMedicine,
-        amount,
-        frequency,
-        doses,
-        reminder,
-      });
+    // Ensure that the amount is a positive number
+    if (amount <= 0) {
+      return res
+        .status(400)
+        .json({ message: "Amount must be greater than zero" });
     }
 
-    res.json({ msg: "Medicine data saved successfully" });
+    // Create new medicine
+    const medicine = await Medicine.create({
+      userId,
+      medicineName,
+      selectedMedicine,
+      amount,
+      frequency,
+      doses,
+      reminder,
+    });
+
+    res.json({ msg: "Medicine data saved successfully", medicine });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server Error" });
