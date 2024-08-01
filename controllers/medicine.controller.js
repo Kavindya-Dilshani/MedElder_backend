186ae7e4c19b366a11048447b104e963430f6dec
Kavindya Dilshani <ranaweerakdilshani@gmail.com>
2024-08-01 23:31:47 +0530

    remove comment and add new validation for amount

--- controllers/medicine.controller.js
+++ controllers/medicine.controller.js
@@ -1,3 +1,69 @@
+// import Medicine from "../models/Medicine.js";
+
+// const getAllMedicine = async (req, res) => {
+//   try {
+//     const medicines = await Medicine.find();
+//     res.status(200).json(medicines);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+// const saveMedicine = async (req, res) => {
+//   console.log(req.body);
+//   try {
+//     const {
+//       userId,
+//       medicineName,
+//       selectedMedicine,
+//       amount,
+//       frequency,
+//       doses,
+//       reminder,
+//     } = req.body;
+
+//     if (
+//       !userId ||
+//       !medicineName ||
+//       !selectedMedicine ||
+//       !amount ||
+//       !frequency ||
+//       !doses ||
+//       !reminder
+//     ) {
+//       return res.status(400).json({ message: "Data not provided" });
+//     }
+
+//     // Ensure that the amount is a positive number
+//     if (amount <= 0) {
+//       return res
+//         .status(400)
+//         .json({ message: "Amount must be greater than zero" });
+//     }
+
+//     // Create new medicine
+//     const medicine = await Medicine.create({
+//       userId,
+//       medicineName,
+//       selectedMedicine,
+//       amount,
+//       frequency,
+//       doses,
+//       reminder,
+//     });
+
+//     res.json({ msg: "Medicine data saved successfully", medicine });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: "Server Error" });
+//   }
+// };
+
+// const medicineController = { getAllMedicine, saveMedicine };
+
+// export default medicineController;
+
+
 import Medicine from "../models/Medicine.js";
 
 const getAllMedicine = async (req, res) => {
@@ -36,9 +102,7 @@ const saveMedicine = async (req, res) => {
 
     // Ensure that the amount is a positive number
     if (amount <= 0) {
-      return res
-        .status(400)
-        .json({ message: "Amount must be greater than zero" });
+      return res.status(400).json({ message: "Amount must be greater than zero" });
     }
 
     // Create new medicine
